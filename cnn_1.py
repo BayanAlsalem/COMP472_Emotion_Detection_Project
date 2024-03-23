86d1b7fdb95f21f2e9891827511b08cab7f1ea7a
BayanAlsalem <b_alsa@live.concordia.ca>
2024-03-23 12:58:15 -0400

    Created CNN_Module_1_twoLayers class

---
+++ cnn_1.py
@@ -0,0 +1,34 @@
+import torch.nn as nn
+
+class CNN_Module_1_twoLayers(nn.Module):
+    def __init__(self, num_classes):
+        super(CNN_Module_1_twoLayers, self).__init__()
+        self.conv_layer = nn.Sequential(
+            # Conv2d creates a 2-dimensional convolutional layer for processing 2D input data (images)
+            nn.Conv2d(in_channels=3, out_channels=32, kernel_size=3, padding=1),
+            nn.BatchNorm2d(32),
+            nn.LeakyReLU(inplace=True),
+            nn.Conv2d(in_channels=32, out_channels=32, kernel_size=3, padding=1),
+            nn.BatchNorm2d(32),
+            nn.LeakyReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+        )
+
+        self.fc_layer = nn.Sequential(
+            nn.Dropout(p=0.1),
+            nn.Linear(32 * 56 * 56, 1000),  # Assuming output size after max pooling is 56x56
+            nn.ReLU(inplace=True),
+            nn.Linear(1000, 512),
+            nn.ReLU(inplace=True),
+            nn.Dropout(p=0.1),
+            nn.Linear(512, num_classes)
+        )
+
+    def forward(self, x):
+        # Convolutional layers
+        x = self.conv_layer(x)
+        # Flatten
+        x = x.view(x.size(0), -1)
+        # Fully connected layers
+        x = self.fc_layer(x)
+        return x
