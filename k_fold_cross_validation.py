9edf330359cbdd7c1ac76062ae544a91aaa1a385
BayanAlsalem <b_alsa@live.concordia.ca>
2024-04-15 15:06:25 -0400

    Create the kFold file and added the CNN class

---
+++ k_fold_cross_validation.py
@@ -0,0 +1,42 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+from sklearn.model_selection import StratifiedKFold
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+from torchvision import datasets, transforms
+from torch.utils.data import Subset, DataLoader
+
+# The CNN architecture
+class CNN_VAR_2(nn.Module):
+    def __init__(self, num_classes):
+        super(CNN_VAR_2, self).__init__()
+
+        self.conv0 = nn.Conv2d(3, 32, kernel_size=3, stride=2, padding=1)
+        self.bn0 = nn.BatchNorm2d(32)
+
+        self.conv1 = nn.Conv2d(32, 32, kernel_size=3, stride=1, padding=1)
+        self.bn1 = nn.BatchNorm2d(32)
+
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1)
+        self.bn2 = nn.BatchNorm2d(64)
+
+        self.conv3 = nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1)
+        self.bn3 = nn.BatchNorm2d(128)
+
+        self.fc1 = nn.Linear(6272, 1024)
+        self.fc2 = nn.Linear(1024, num_classes)
+
+        self.pool = nn.MaxPool2d(2, 2)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.bn0(self.conv0(x))))
+        x = self.pool(F.relu(self.bn1(self.conv1(x))))
+        x = self.pool(F.relu(self.bn2(self.conv2(x))))
+        x = self.pool(F.relu(self.bn3(self.conv3(x))))
+
+        x = x.view(-1, 6272)
+        x = F.relu(self.fc1(x))
+        x = F.dropout(x, 0.5, training=self.training)
+        x = self.fc2(x)
+        return x
